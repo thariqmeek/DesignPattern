192a9ec157a7d5e5b03bc4c08a15188b461b09a5
thariqmeek <mohamedthariq0705@gmail.com>
2024-10-23 00:42:31 +0530

    Comments and ReadMe is updated for Singleton Class

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DesignPattern
 A Repository to explain Design patterns with Practical Implementation
+
+## Table of Contents
+- [Singleton Pattern](#Singleton Pattern)
+
+
+## Singleton Pattern
+
+### What is Singleton Class
+-   Singleton means one i.e. Only one instance of the class can be created
+
+### Steps to create a Singleton Class: 
+- In the SingletonService class we can see for class Abc() a static instance object (obj) is created for that class
+- For that class the default constructor is made private ,so that no object can be created for that class
+-  After that, static method (getInstance()) is created for that class which will return the object(obj) of Abc
+
+
--- src/main/java/com/thariq/SingletonPattern/SingletonService.java
+++ src/main/java/com/thariq/SingletonPattern/SingletonService.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class SingletonService {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
-        Abc obj  = new Abc();
+       /* this type of object cannot be created for Siingleton class, since the default constructor is made private
+       Abc obj  = new Abc();
+       */
 
         Abc obj1 = Abc.getInstance();
         Abc obj2 = Abc.getInstance();
@@ -15,11 +17,10 @@ public class SingletonService {
     }
 }
 class Abc{
-        static  Abc obj = new Abc();
-        private Abc(){
+        static  Abc obj = new Abc();  /* Static instance of the class */
+        private Abc(){  }   /* the default constructor is made private */
 
-        }
-
+    /* static method is created for that class to return instance of that class */
         public static Abc getInstance(){
             return obj;
         }
